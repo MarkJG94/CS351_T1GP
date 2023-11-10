4d3cb403fbc38aeb4a4a0c36e8b0d893f3789e9b
martin-aitken-84 <martin_aitken_84@yahoo.co.uk>
2023-11-10 15:26:10 +0000

    Added JUnit 8.5.1 to iml file Added MarketplaceTest Updated Marketplace.java to add implementation for addFunds and deductFunds Deleted Item.java

--- src/Marketplace.java
+++ src/Marketplace.java
@@ -3,37 +3,34 @@ import java.util.List;
 
 public class Marketplace implements Market {
 
-
+    Server server = new Server();
+    ArrayList<User> userList = server.getUserList();
     ArrayList<Resource> marketResources = new ArrayList<Resource>();
-    ArrayList<Item> marketItems = new ArrayList<Item>();
 
-
-    public boolean addItem(int itemId, int quantity) {
+    @Override
+    public boolean addResource(int resourceId, int quantity) {
         if (quantity > 0) {
             /*Check for item already existing*/
-            for (Item item : marketItems) {
-                if (item.getId() == itemId) {
+            for (Resource resource : marketResources) {
+                if (resource.getId() == resourceId) {
                     /*If exists, update the quantity and return true*/
-                    item.setQuantity(item.getQuantity() + quantity);
+                    resource.setQuantity(resource.getQuantity() + quantity);
                     return true;
                 }
             }
-            /*If not existing, create new item and return true*/
-            Item new_item = new Item(getItemDetails(itemId));
-            marketItems.add(new_item);
-            return true;
         }
         /*Can't add negative number*/
         return false;
     }
 
-    public boolean removeItem(int itemId, int quantity) {
+    @Override
+    public boolean removeResource(int resourceId, int quantity) {
         if (quantity > 0) {
-            /*Check for item already existing*/
-            for (Item item : marketItems) {
-                if (item.getId() == itemId) {
+            /*Check for Item already existing*/
+            for (Resource resource : marketResources) {
+                if (resource.getId() == resourceId) {
                     /*If exists, update the quantity and return true*/
-                    item.setQuantity(item.getQuantity() - quantity);
+                    resource.setQuantity(resource.getQuantity() - quantity);
                     return true;
                 } else {
                     /*Item doesn't exist*/
@@ -45,12 +42,100 @@ public class Marketplace implements Market {
         return false;
     }
 
-    public boolean addFinds
 
 
+    @Override
+    public boolean notifyUser(String username, int quantity, String resourceName) {
+        return false;
+    }
 
+    @Override
+    public boolean validateCurrency(int amount) {
+        return false;
+    }
 
+    @Override
+    public int calculateTotal(int quantity, int resourceID) {
+        return 0;
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return false;
+    }
+
+    @Override
+    public int getResourceDetails(int resourceID) {
+        return 0;
+    }
+
+    @Override
+    public int getFunds() {
+        return 0;
+    }
+
+    @Override
+    public int getFunds(String username) {
+        for (User user : userList) {
+            if (user.username.equals(username)) {
+                return user.funds;
+            } else {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public ArrayList<Integer> getUserInventory() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Integer> getUserInventory(String username) {
+        return null;
+    }
+
+    @Override
+    public int addFunds(String destination_username, int amount) {
+        if (amount > 0) {
+            for (User user : userList) {
+                /*If user exists, add funds*/
+                if (user.username.equals(destination_username)) {
+                    user.funds = user.funds + amount;
+                    return user.funds;
+                } else{
+                    /*If user doesn't exist, return error*/
+                    return -1;
+                }
+            }
+        }
+        /*Can't add negative number, return error*/
+        return -1;
+    }
+
+    @Override
+    public int deductFunds(String destination_username, int amount) {
+        if (amount > 0) {
+            for (User user : userList) {
+                /*If user exists, deduct funds*/
+                if (user.username.equals(destination_username)) {
+                    user.funds = user.funds - amount;
+                    return user.funds;
+                } else{
+                    /*If user doesn't exist, return error*/
+                    return -1;
+                }
+            }
+        }
+        /*Can't deduct negative number, return error*/
+        return -1;
+    }
+
+    @Override
+    public boolean transferFunds(String source, String destination, int amount) {
+        return false;
+    }
 
 
 }
-
--- src/Server.java
+++ src/Server.java
@@ -4,4 +4,8 @@ public class Server {
     private ArrayList<Resource> resources;
     private ArrayList<User> userList;
     private Marketplace marketPlace;
+
+    public ArrayList<User>getUserList(){
+        return userList;
+    }
 }
