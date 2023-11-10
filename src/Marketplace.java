d953eb974e974e0e7d3aa05f859a286d5156a32b
martin-aitken-84 <martin_aitken_84@yahoo.co.uk>
2023-11-10 13:01:42 +0000

    Added addItem and removeItem to Marketplace.java Added new Item class Added method to Market interface

--- src/Marketplace.java
+++ src/Marketplace.java
@@ -1,7 +1,56 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class Marketplace  {
+public class Marketplace implements Market {
 
-    ArrayList<Resource> marketResources;
+
+    ArrayList<Resource> marketResources = new ArrayList<Resource>();
+    ArrayList<Item> marketItems = new ArrayList<Item>();
+
+
+    public boolean addItem(int itemId, int quantity) {
+        if (quantity > 0) {
+            /*Check for item already existing*/
+            for (Item item : marketItems) {
+                if (item.getId() == itemId) {
+                    /*If exists, update the quantity and return true*/
+                    item.setQuantity(item.getQuantity() + quantity);
+                    return true;
+                }
+            }
+            /*If not existing, create new item and return true*/
+            Item new_item = new Item(getItemDetails(itemId));
+            marketItems.add(new_item);
+            return true;
+        }
+        /*Can't add negative number*/
+        return false;
+    }
+
+    public boolean removeItem(int itemId, int quantity) {
+        if (quantity > 0) {
+            /*Check for item already existing*/
+            for (Item item : marketItems) {
+                if (item.getId() == itemId) {
+                    /*If exists, update the quantity and return true*/
+                    item.setQuantity(item.getQuantity() - quantity);
+                    return true;
+                } else {
+                    /*Item doesn't exist*/
+                    return false;
+                }
+            }
+        }
+        /*Can't deduct negative number*/
+        return false;
+    }
+
+    public boolean addFinds
+
+
+
+
+
 
 }
+
