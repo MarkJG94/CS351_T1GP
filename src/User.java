9bb4265e04b335085397c8940b81482e063f3903
Mark Gallacher <mark.gallacher94@gmail.com>
2023-11-11 14:48:37 +0000

    Updates to User and Marketplace

--- src/User.java
+++ src/User.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.util.*;
 
 //get online users
-public class User implements Market {
+public class User {
 
     ArrayList<Resource> userResources;
     String username;
@@ -22,238 +22,65 @@ public class User implements Market {
         this.userResources = userResources;
         this.funds = funds;
     }
-
-    public ArrayList<String> getUserInventory(){
-        //reads in file and prints all users inventory
-        try {
-            BufferedReader  bufferedReader = new BufferedReader(new FileReader("UserDetails.txt"));
-            ArrayList<String> userInventory = new ArrayList<>();
-
-            String line = bufferedReader.readLine();
-            while (line !=null){
-                userInventory.add(line);
-                line = bufferedReader.readLine();
-            }
-            bufferedReader.close();
-            System.out.println(userInventory);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return getUserInventory();
+    
+    public ArrayList<Resource> getUserInventory() {
+        return userResources;
 
     }
-
-    @Override
-    public ArrayList<String> getUserInventory(String username) {
-        //search for username then print users inventory
-        try {
-            Scanner scanner = new Scanner(new File("UserDetails.txt"));
-            while (scanner.hasNext()){
-                String line = scanner.nextLine();
-                if (line.contains(username)){
-                    System.out.println(line);
-                }
+    
+    public int getResourceIndex(int resourceID) {
+        for (Resource resource : userResources) {
+            if (resource.getId() == resourceID) {
+                return userResources.indexOf(resource);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
-        return getUserInventory();
-
+        return -1;
     }
 
-    @Override
-    //change parameters?
-    public int getResourceQuantity() {
-        try {
-            Scanner scanner = new Scanner(new File("UserDetails.txt"));
-            while (scanner.hasNext()){
-                String line = scanner.nextLine();
-                if (line.contains(username)){
-                    String[] parts = line.split(",");
-                    System.out.println(line);
-                    username = parts[0].trim();
-                    String password = parts[1].trim();
-                    int currency = Integer.parseInt(parts[2].trim());
-                    String status = parts[3].trim();
-                    String accountType = parts[4].trim();
-                    int wood = Integer.parseInt(parts[5].trim());
-                    int stone = Integer.parseInt(parts[6].trim());
-                    int iron = Integer.parseInt(parts[7].trim());
-                    int gold = Integer.parseInt(parts[8].trim());
-                    int silver = Integer.parseInt(parts[9].trim());
-                    funds=funds+currency;
-                    System.out.println(wood);
-                    System.out.println(stone);
-                    System.out.println(iron);
-                    System.out.println(gold);
-                    System.out.println(silver);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
+    
+    public int getResourceQuantity(int resourceID) {
+        return userResources.get(getResourceIndex( resourceID )).getQuantity();
     }
 
-    @Override
+    
     public int addFunds(String username, int amount) {
-        //read file, add funds, delete row in file and then write back to file
-        //what are we adding?
-        try {
-            Scanner scanner = new Scanner(new File("UserDetails.txt"));
-            while (scanner.hasNext()){
-                String line = scanner.nextLine();
-                if (line.contains(username)){
-                    String[] parts = line.split(",");
-                    System.out.println(line);
-                    username = parts[0].trim();
-                    String password = parts[1].trim();
-                    int currency = Integer.parseInt(parts[2].trim());
-                    String status = parts[3].trim();
-                    String accountType = parts[4].trim();
-                    int wood = Integer.parseInt(parts[5].trim());
-                    int stone = Integer.parseInt(parts[6].trim());
-                    int iron = Integer.parseInt(parts[7].trim());
-                    int gold = Integer.parseInt(parts[8].trim());
-                    int silver = Integer.parseInt(parts[9].trim());
-                    funds=funds+currency;
-
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        
+        if (amount > 0 && username.equals( this.username ))
+        {
+            funds = funds + amount;
+            return funds;
         }
-
-        funds=funds+amount;
-        return funds;
-
+        return -1;
     }
+    
 
-
-    @Override
+    
     public int deductFunds(String username, int amount) {
-        try {
-            Scanner scanner = new Scanner(new File("UserDetails.txt"));
-            while (scanner.hasNext()){
-                String line = scanner.nextLine();
-                if (line.contains(username)){
-                    String[] parts = line.split(",");
-                    System.out.println(line);
-                    username = parts[0].trim();
-                    String password = parts[1].trim();
-                    int currency = Integer.parseInt(parts[2].trim());
-                    String status = parts[3].trim();
-                    String accountType = parts[4].trim();
-                    int wood = Integer.parseInt(parts[5].trim());
-                    int stone = Integer.parseInt(parts[6].trim());
-                    int iron = Integer.parseInt(parts[7].trim());
-                    int gold = Integer.parseInt(parts[8].trim());
-                    int silver = Integer.parseInt(parts[9].trim());
-                    funds=funds+currency;
-
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        if (amount > 0 && username.equals( this.username ) && validateCurrency( amount ))
+        {
+            funds = funds - amount;
+            return funds;
         }
-        funds = funds - amount;
-        return funds;
-
+        return -1;
     }
 
 
-    @Override
+    
     public boolean validateCurrency(int amount) {
-        if (funds > 0){
+        if (funds >= amount){
             return true;
         }
         return false;
     }
-
-    public int getFunds(){
-        return funds;
-    }
-
-    @Override
-    public int getFunds(String username) {
-        try {
-            Scanner scanner = new Scanner(new File("UserDetails.txt"));
-            while (scanner.hasNext()){
-                String line = scanner.nextLine();
-                if (line.contains(username)){
-                    String[] parts = line.split(",");
-                    System.out.println(line);
-                    username = parts[0].trim();
-                    String password = parts[1].trim();
-                    int currency = Integer.parseInt(parts[2].trim());
-                    String status = parts[3].trim();
-                    String accountType = parts[4].trim();
-                    int wood = Integer.parseInt(parts[5].trim());
-                    int stone = Integer.parseInt(parts[6].trim());
-                    int iron = Integer.parseInt(parts[7].trim());
-                    int gold = Integer.parseInt(parts[8].trim());
-                    int silver = Integer.parseInt(parts[9].trim());
-                    funds=funds+currency;
-
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    
+    public int getFunds() {
         return funds;
     }
 
     public String getUsername(){
-        try {
-            Scanner scanner = new Scanner(new File("UserDetails.txt"));
-            while (scanner.hasNext()){
-                String line = scanner.nextLine();
-                if (line.contains(username)){
-                    String[] parts = line.split(",");
-                    System.out.println(line);
-                    username = parts[0].trim();
-                    String password = parts[1].trim();
-                    int currency = Integer.parseInt(parts[2].trim());
-                    String status = parts[3].trim();
-                    String accountType = parts[4].trim();
-                    int wood = Integer.parseInt(parts[5].trim());
-                    int stone = Integer.parseInt(parts[6].trim());
-                    int iron = Integer.parseInt(parts[7].trim());
-                    int gold = Integer.parseInt(parts[8].trim());
-                    int silver = Integer.parseInt(parts[9].trim());
-                    funds=funds+currency;
-
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
         return username;
-
     }
-
-
-    public boolean transferFunds(String source, String destination, int amount){
-        return true;
-    }
-
-    @Override
-    public boolean userExists(String username) {
-        try {
-            Scanner scanner = new Scanner(new File("UserDetails.txt"));
-            while (scanner.hasNext()){
-                String line = scanner.nextLine();
-                if (line.contains(username)){
-                    return true;
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    @Override
+    
     public boolean addItem(String itemID, int quantity) {
         //read file, add to wood or iron ... quantity
         boolean added = false;
@@ -307,7 +134,7 @@ public class User implements Market {
         return added;
     }
 
-    @Override
+    
     public boolean removeItem(String itemID, int quantity) {
         //read file, add to wood or iron ... quantity
         boolean added = false;
