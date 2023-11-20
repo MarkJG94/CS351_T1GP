e12c354b7ec643434b28046338f5df790e7823d1
Andrew <92223097+Iamandyf96@users.noreply.github.com>
2023-11-20 18:03:56 +0000

    Moved the source files for the user and market to a subfolder and included "[SERVER]" for any notification messages sent to a user.

--- src/Client.java
+++ src/Client.java
@@ -142,11 +142,6 @@ public class Client extends InputReader{
         operator.start();
     }
 
-    public static void infoBox(String infoMessage, String titleBar)
-    {
-        JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
-    }
-
     private void endServerResponseQueue(){
         operator.interrupt();
     }
